1a818bfbaaa60d937380bf82d9619fffa2930aa2
widyaputri261 <widyaputri.pratama@gmail.com>
2022-04-27 18:31:42 +0700

    first commit

---
+++ app/src/main/java/com/example/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,12 @@
+package com.example.jsonmoviedb;
+
+public class ApiEndpoint {
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "";
+    public static String LANGUAGE = "&language=en-US";
+    public static  String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discoverxmovie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780";
+}
